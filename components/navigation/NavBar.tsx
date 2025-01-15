4df75f44d25cbbfde59b1b310f4e1f3237e7edc2
nischal puri <mrnischalpuri@gmail.com>
2025-01-15 15:46:34 +0545

    [Signin/signOut] setup

--- components/navigation/NavBar.tsx
+++ components/navigation/NavBar.tsx
@@ -1,15 +1,32 @@
 "use client"
 
+import { useCurrentUser } from "@/hooks/user"
 import { UserButton } from "../user-button/UserButton"
+import { LogIn } from "lucide-react"
+import { useRouter } from "next/navigation"
 
 export default function NavBar() {
+  const user = useCurrentUser()
+  const router = useRouter()
   return (
     <header className="bg-slate-500 py-4">
-      <nav>
-        <ul className="flex justify-between">
+      <nav className="container">
+        <ul className="flex  justify-between">
           <li>Logo</li>
           <li>
-            <UserButton />
+            {!user ? (
+              <div
+                onClick={() => router.push("/api/auth/signin")}
+                className="bg-primary flex items-center gap-1.5 p-1 rounded-md"
+              >
+                <span>
+                  <LogIn className="w-4 h-4" />
+                </span>
+                Login
+              </div>
+            ) : (
+              <UserButton />
+            )}
           </li>
         </ul>
       </nav>
